e402115a28bc676a1744e82248906b7ae47846c0
Matheus Braga <sbmatheus20@gmail.com>
2025-12-04 21:45:14 -0300

    Constantes

---
+++ aula06/const.js
@@ -0,0 +1,6 @@
+// String = Text | Number = Numero
+const primeiroNumero = 5
+const segundoNumero = 10
+const resultado = primeiroNumero * segundoNumero
+const resultadoDuplicado = resultado * 2
+console.log(resultadoDuplicado)
