ec34765650347e0f7e8c2a24a9c2c83510a6e9ba
Matheus Braga <sbmatheus20@gmail.com>
2025-12-05 22:44:34 -0300

    alert, confirm e prompt

---
+++ aula10/script.js
@@ -0,0 +1,5 @@
+const n1 = prompt("Digfite o primeiro número:");
+const n2 = prompt("Digite o segundo número:");
+
+const resultado = Number(n1) + Number(n2);
+alert(`O resultado da soma é: ${resultado}`);
