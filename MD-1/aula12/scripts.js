adec2d65f5ec504bd55e4fccbcd0e9dba8e5d703
Matheus Braga <sbmatheus20@gmail.com>
2025-12-06 23:23:52 -0300

    Exercicios com Strings

---
+++ MD-1/aula12/scripts.js
@@ -0,0 +1,5 @@
+//          0123456789
+let umaString = 'O rato roeu a roupa do rei de Roma.'
+
+console.log(umaString.toUpperCase()) // Deixa maiusculo
+console.log(umaString.toLowerCase()) // Deixa minusculo
