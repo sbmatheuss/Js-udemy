c025cb994347857207ce58c5c6e484a090a5e8e0
Matheus Braga <sbmatheus20@gmail.com>
2025-12-08 00:33:43 -0300

    Arrays

---
+++ MD-1/aula16/script.js
@@ -0,0 +1,25 @@
+//                0       1         2
+const alunos = ['Luiz', 'Maria', 'João']
+
+
+console.log(typeof alunos)
+console.log(alunos instanceof Array)
+
+
+
+// console.log(alunos.slice(0, 2)) // Corta o array
+
+
+// alunos.unshift('Ana') // Adiciona no início do array
+// alunos.push('Pedro') // Adiciona no final do array
+
+// alunos.pop() // Remove do final do array
+// alunos.shift() // Remove do início do array
+
+// delete alunos[1] // Remove o elemento, mas deixa um "buraco" no array
+
+// console.log(alunos)
+// console.log(alunos[0]) // Acessa o elemento do índice 0
+// console.log(alunos[2]) // Acessa o elemento do índice 2
+
+
