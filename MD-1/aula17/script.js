a3beda33cd325d414c56716c38b32ab6c93e502e
Matheus Braga <sbmatheus20@gmail.com>
2025-12-08 00:57:53 -0300

    Funções

---
+++ MD-1/aula17/script.js
@@ -0,0 +1,17 @@
+
+
+function soma (x, y) {
+  const resultado = x + y
+  return resultado 
+}
+
+  const resultado = soma(4, 2)
+  console.log(resultado)
+
+const raiz = function (n) {
+  return n ** 0.5
+}
+
+console.log(raiz(9))
+console.log(raiz(16))
+console.log(raiz(25))
