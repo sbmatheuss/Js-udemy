ba0aedfa9128b7ebdec3a87a05ff09d5f0635496
Matheus Braga <sbmatheus20@gmail.com>
2025-12-07 23:57:29 -0300

    Exercicios com numbers e Math

---
+++ MD-1/aula14/script.js
@@ -0,0 +1,9 @@
+// let num2 = Math.floor(num1)
+// let num2 = Math.ceil(num1)
+// let num2 = Math.round(num1)
+// console.log(Math.max(10, 5, 20, 3, 7))
+// console.log(Math.min(10, 5, 20, 3, 7))
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5)
+
+
+console.log(!!(100 / 0))
