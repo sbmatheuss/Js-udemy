096f031a407e431abf40d7277b3a6fa418743e4b
Matheus Braga <sbmatheus20@gmail.com>
2025-12-09 22:07:30 -0300

    Operadores Lógicos

---
+++ MD-2/aula01/script.js
@@ -0,0 +1,19 @@
+/* 
+Operadores de comparação
+
+< menor que
+> maior que
+<= menor ou igual que
+>= maior ou igual que
+== igual a
+=== estritamente igual a
+!= diferente de
+!== estritamente diferente de
+
+
+*/
+
+const num1 = 10;
+const num2 = 10;
+const comp = num1 === num2; // false
+console.log(comp)
