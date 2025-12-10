5112ddd54395ec381cf9811b169c2c7c8e125548
Matheus Braga <sbmatheus20@gmail.com>
2025-12-09 22:38:03 -0300

    Operadores Lógicos

---
+++ MD-2/aula02/script.js
@@ -0,0 +1,17 @@
+/*
+
+Operadores Lógicos
+
+&& (AND) - Retorna true se ambos os operandos forem true
+|| (OR) - Retorna true se pelo menos um dos operandos for true
+! (NOT) - Inverte o valor lógico do operando
+
+*/
+
+// const usuario = 'Luiz' // form usuário digitiou
+// const senha = '123456' // form usuário digitou
+
+//                          True                 False
+// const vaiLogar = usuario === 'Luiz' && senha === '12345'
+
+console.log(!!true)
