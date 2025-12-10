8b816fff156c7048fa0caaedd1cf23f3f5762a2c
Matheus Braga <sbmatheus20@gmail.com>
2025-12-09 23:13:52 -0300

    Avaliacao de short-circuit

---
+++ MD-2/aula02/index.js
@@ -0,0 +1,21 @@
+/*
+&& -> false && true -> "o valor mesmo"
+|| -> true && false -> vai retornar "o valor verdadeiro"
+
+
+FALSY
+false
+0
+'' "" ``
+
+null / undefined
+NaN
+*/
+
+const a = 0
+const b = null
+const c = false
+const d = false
+const e = NaN
+
+console.log(a || b || c || d || e)
