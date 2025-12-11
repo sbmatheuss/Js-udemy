24da925a92f3fed27148cb9e0a2c59db66c9cbe1
Matheus Braga <sbmatheus20@gmail.com>
2025-12-10 22:36:06 -0300

    if, else if e else

---
+++ MD-2/aula03/script.js
@@ -0,0 +1,12 @@
+// If pode ser usado sozinho
+// Sempe que utilizo a palavra else, preciso de um if antes
+// Eu posso ter vários else ifs na checagem
+// Só posso ter um else na checagem
+// Podemos usar condições sem else if, utilizando apenas if e else
+
+
+  const tenhoGrana = true
+
+  if(tenhoGrana) {
+    console.log('Vou sair de casa')
+  }
