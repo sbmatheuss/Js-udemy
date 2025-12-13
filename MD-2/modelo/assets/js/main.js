60279ecaf4d6da4a21ab65238a0e67e045d0a012
Matheus Braga <sbmatheus20@gmail.com>
2025-12-12 23:30:53 -0300

    criando a lógica do javascript

---
+++ MD-2/modelo/assets/js/main.js
@@ -0,0 +1,41 @@
+// Capturar evento de submit do formulário
+const form = document.querySelector('#formulario')
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
+  const inputPeso = e.target.querySelector('#peso')
+  const inputAltura = e.target.querySelector('#altura')
+
+  const peso = Number(inputPeso.value)
+  const altura = Number(inputAltura.value)
+
+  if(!peso) {
+    setResultado('Peso inválido', false)
+    return
+  }
+
+  if(!altura) {
+    setResultado('Altura inválida', false)
+    return
+  }
+
+
+  console.log('Cheguei aqui')
+})
+
+
+function criaP () {
+  const p = document.createElement('p')
+  return p
+}
+
+
+function setResultado (msg, isValid) {
+  const resultado = document.querySelector('#resultado')
+  resultado.innerHTML = ''
+  
+
+  const p = criaP()
+  p.innerHTML = msg
+  resultado.appendChild(p)
+}
