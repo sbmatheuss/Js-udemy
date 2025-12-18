436678c5da68e878e72ee4c01c21f190efe09584
Matheus Braga <sbmatheus20@gmail.com>
2025-12-17 22:55:48 -0300

    switch/case

---
+++ MD-2/aula06/script.js
@@ -0,0 +1,38 @@
+function getDiaSemanaTexto(diaSemana) {
+  let diaSemanaTexto;
+
+  switch (diaSemana) {
+    case 0:
+      diaSemanaTexto = 'Domingo';
+      return diaSemanaTexto;
+    case 1:
+      diaSemanaTexto = 'Segunda-feira';
+      return diaSemanaTexto;
+    case 2:
+      diaSemanaTexto = 'Terça-feira';
+      return diaSemanaTexto;
+    case 3:
+      diaSemanaTexto = 'Quarta-feira';
+      return diaSemanaTexto;
+    case 4:
+      diaSemanaTexto = 'Quinta-feira';
+      return diaSemanaTexto;
+    case 5:
+      diaSemanaTexto = 'Sexta-feira';
+      return diaSemanaTexto;
+    case 6:
+      diaSemanaTexto = 'Sábado';
+      return diaSemanaTexto;
+    default:
+      diaSemanaTexto = '';
+      return diaSemanaTexto;
+  }
+  
+
+}
+
+const data = new Date('1987-04-21 00:00:00');
+let diaSemana = data.getDay();
+let diaSemanaTexto = getDiaSemanaTexto(diaSemana);
+
+console.log(diaSemana, diaSemanaTexto)
