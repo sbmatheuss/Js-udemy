cb4a6b38baf8178a7a044087519e3e45a66a958d
Matheus Braga <sbmatheus20@gmail.com>
2025-12-23 21:38:53 -0300

    for of

---
+++ MD-2/aula13/script.js
@@ -0,0 +1,14 @@
+const frutas = ["maçã", "banana", "laranja"];
+
+for (let fruta of frutas) { 
+  // let frutas > variável que vai receber cada valor do array
+  // of fruas > indica de onde vêm esses valores
+    console.log(fruta);
+}
+
+
+// For clássico - Geralmente com iteráveis (array ou string)
+// For in - Retorna o índice ou chave (string, array ou objetos)
+// For of - Retorna o valor em si (iteráveis: array ou string)
+
+
